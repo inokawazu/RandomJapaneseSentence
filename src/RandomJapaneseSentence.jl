c5d84d7a7e86015ffc931673530789b8893e3642
Markus-Amano <markus.a.amano@gmail.com>
2022-03-19 19:28:35 +0800

    added data files to code

--- src/RandomJapaneseSentence.jl
+++ src/RandomJapaneseSentence.jl
@@ -3,4 +3,12 @@ module RandomJapaneseSentence
 # TODO: import japanese sentences English and Japanese version
 # TODO: parse sentences to table
 
+"""
+to_data_dir makes a valid path to data file
+"""
+to_data_dir(data_name) = joinpath([@__DIR__,"..","data",data_name])
+
+const TATOEBA_FILE  = to_data_dir("tatoeba_EN_JP.tsv")
+const TANAKA_C_FILE = to_data_dir("tanaka_corpus.txt")
+
 end # module
